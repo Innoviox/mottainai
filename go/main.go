62bbfb479710a8a8fa7d3af4510e07855ab50577
Simon Chervenak <sarras305@gmail.com>
2020-02-05 19:42:20 -0500

    museum

--- go/main.go
+++ go/main.go
@@ -9,6 +9,17 @@ var mat = NewMaterials()
 type Card struct {
 	mat  Material
 	name string
+	text string
+}
+
+type Museum struct {
+	sales      []Card
+	helpers    []Card
+	gift_shop  []Card
+	gallery    []Card
+	work_bench []Card
+
+	task Card
 }
 
 func main() {
